b76996609ec1fc9533e0fb439ae74b2b67811722
Julian Hoyos <jhoyos135@gmail.com>
2019-01-09 18:23:17 -0500

    look ahead feature

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 /*
-//api used: https://www.openbrewerydb.org
+// api used: https://www.openbrewerydb.org
 */
 
-window.onload = () => {
+window.addEventListener('DOMContentLoaded', () => {
 
-    let submit = document.querySelector('.submit');
-    var form = document.getElementsByName('contact-form')[0];
 
-submit.onclick = (e) => {
-        e.preventDefault()
+// let submit = document.querySelector('.submit');
+let name_input = document.querySelector('#full_name');
+var form = document.getElementsByName('contact-form')[0];
+
+let submitHandle = () => {
+
+    // e.preventDefault()
 
     const fetch_brew = async () => {
 
         let names = document.querySelector("#full_name");
-        let name = `?by_name=${names.value.toLowerCase()}`
+        let name = `?by_name=${names.value.toLowerCase()}`;
 
         let states = document.getElementById("states");
-        let state = `&by_state=${states.options[states.selectedIndex].text}`
-
+        let state = `&by_state=${states.options[states.selectedIndex].text}`;
 
         let url = `https://api.openbrewerydb.org/breweries/${name}${state}`;
 
-        console.log(url);
-
+        // console.log(url);
+        
         let res = await fetch(url);
         let breweries = await res.json();
 
-        for(let i in breweries) {
+    for(let i in breweries) {
         let json = breweries[i];
 
-            console.log(json)
+            // console.log(json)
 
            let info =  document.querySelector("#information");
            let name = json.name;
@@ -45,7 +47,8 @@ submit.onclick = (e) => {
            info.innerHTML += `
         <div class="card mt-3 mb-3">
             <div class="card-header">
-                <h4>${json.name}</h4> <span>Type:  ${brewery_type} </span>
+                <h4> ${name} </h4> 
+                <span>Type: ${brewery_type} </span>
             </div>
             <div class="card-body">
                 <h5 class="card-title">
@@ -65,28 +68,44 @@ submit.onclick = (e) => {
             </div>
         </div>
            `;
-        };
+    };
       
     };
 
     fetch_brew();
 
-    form.reset();
+    // form.reset();
 
     document.querySelector("#information").innerHTML = '';
 
 };
 
-        // success();
+// submit.addEventListener('click', submitHandle);
+name_input.addEventListener('keyup', () => {
 
-};
+    submitHandle();
+    success();
+    
+});
 
-// let name = document.querySelector("#full_name");
+});
 
-// function success() {
-//     if(name.value.length == 0 || name.value.length === ''  ) { 
-//            document.querySelector('.submit').disabled = true; 
-//        } else { 
-//            document.querySelector('.submit').disabled = false;
-//        }
-//    }
+let name = document.querySelector("#full_name");
+let information = document.querySelector("#information");
+let states = document.getElementById("states");
+
+let success = () => {
+    if(name.value.length === 0 || name.value.length === '' ) { 
+
+        information.classList.add('hide');
+        information.classList.remove('show');
+        information.innerHTML = '';
+        
+       } else { 
+
+        information.classList.add('show');
+        information.classList.remove('hide');
+        
+       }
+       
+}
