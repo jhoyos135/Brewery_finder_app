c6abda0c20a0ae73ef0b2d5afb3ed44abefcd903
Julian Hoyos <jhoyos135@gmail.com>
2019-02-01 23:32:18 -0500

    added weather, modals, authentication

--- script.js
+++ script.js
@@ -4,119 +4,139 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-// let submit = document.querySelector('.submit');
 let name_input = document.querySelector('#full_name');
-var form = document.getElementsByName('contact-form')[0];
-let submitHandle = () => {
+let pageNum = 1;
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev');
 
-    // e.preventDefault()
+const fetch_brew = async (name) => {
 
-    const fetch_brew = async () => {
+    let states = document.getElementById("states");
+    let state = `&by_state=${states.options[states.selectedIndex].text}`;
 
-        let names = document.querySelector("#full_name");
-        let name = `?by_name=${names.value.toLowerCase()}`;
+    let url = `https://api.openbrewerydb.org/breweries?by_name=${name.toLowerCase()}${state}&page=${pageNum}&per_page=1&sort=name`;
 
-        let states = document.getElementById("states");
-        let state = `&by_state=${states.options[states.selectedIndex].text}`;
 
-        let url = `https://api.openbrewerydb.org/breweries/${name}${state}`;
+    // console.log(url);
+    
+    let res = await fetch(url);
+    let breweries = await res.json();
 
-        // console.log(url);
-        
-        let res = await fetch(url);
-        let breweries = await res.json();
+for(let i in breweries) {
+    let json = breweries[i];
 
-    for(let i in breweries) {
-        let json = breweries[i];
+        // console.log(json)
 
-            // console.log(json)
+       let info =  document.querySelector("#information");
+       let id = json.id;
+       let name = json.name;
+       let brewery_type = json.brewery_type;
+       let street = json.street;
+       let city = json.city;
+       let state = json.state;
+       let postal = json.postal_code;
+       let phone = json.phone;
+       let website = json.website_url;
+       let address = `${street}, ${city}, ${state}, ${postal}`;
+       let lat = parseFloat(json.latitude);
+       let lon = parseFloat(json.longitude);
+       
 
-           let info =  document.querySelector("#information");
-           let name = json.name;
-           let brewery_type = json.brewery_type;
-           let street = json.street;
-           let city = json.city;
-           let state = json.state;
-           let postal = json.postal_code;
-           let phone = json.phone;
-           let website = json.website_url;
-           let address = `${street}, ${city}, ${state}, ${postal}`;
-
-           info.innerHTML += `
-        <div class="card mt-3 mb-3">
-            <div class="card-header">
-                <h4> ${name} </h4> 
-                <p>Type: ${brewery_type} </p>
-                <p>City: ${city} </p>
+       info.innerHTML += `
+    <div class="card hoverable z-depth-1">
+        <div class="card-title">
+            <h5> ${name} </h5> 
+            <p>Type: ${brewery_type} </p>
+            <p>City: ${city} </p>
+        </div>
+        <div class="card-content">
+            <div class="card-action">
+            <p>
+              <a target="_blank" href="https://www.google.ca/maps/place/${street},+${state},+${postal}/" class="btn indigo"> Address </a>
+            </p>
             </div>
-            <div class="card-body">
-                <div>
-                <h5>
-                  <a target="_blank" href="https://www.google.ca/maps/place/${street},+${state},+${postal}/" class="btn btn-secondary"> Address </a>
-                </h5>
-                </div>
 
-                <div class="mt-2">
-                <h4> <span><a class="btn btn-primary" href="${website}" target="_blank"> Visit Website </a>
-                </h4>
-                </div>
+            <div class="mt-2 card-action">
+            <p> <span><a class="btn indigo modal-trigger" href="${website}" data-target="website-${id}"> Visit Website </a>
+            </p>
+            </div>
 
-                <div class="text-right call">
-                <h5>
-                    <a href="tel://+1${phone}">
-                        <i class="fa fa-phone fa-2x"></i>
-                    </a>
-                </h5> 
-                </div>
+            <div class="text-right call">
+            <h5>
+                <a href="tel://+1${phone}">
+                    <i class="fa fa-phone fa-2x"></i>
+                </a>
+            </h5> 
             </div>
         </div>
-           `;
-    };
-      
-    };
 
-    fetch_brew();
+        <!-- Modals -->
+        <div id="${id}"></div>
+       
+</div>
+       `;
 
-    // form.reset();
 
-    document.querySelector("#information").innerHTML = '';
+       google(lat, lon, name, id, website);
 
 };
+  // setup materialize components
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
 
-// submit.addEventListener('click', submitHandle);
+  console.clear()
+  
+};
+
 name_input.addEventListener('keyup', () => {
-
-    submitHandle();
-    success();
+    
+    pageNum = 1;
+    submitHandle(name_input.value);
     
 });
 
 
 let states = document.getElementById("states");
-// var selectedString = states.options[states.selectedIndex].text;
 
-states.onchange = function(){
-   
-    submitHandle();
-}
+states.onchange = function() {
+    let single_state = states.options[states.selectedIndex].text;
+
+    pageNum = 1;
+    submitHandle(name_input.value);
+    fetch_weather(single_state);
+
+};
+
+// Pagination functionality
+let page_up = () => {
+    pageNum = pageNum + 1;
+};
+
+let page_down = () => {
+    pageNum = pageNum - 1;
+};
+
+next.addEventListener('click', () => {
+    page_up();
+    submitHandle(name_input.value);
 
 });
 
-let name = document.querySelector("#full_name");
-let information = document.querySelector("#information");
+prev.addEventListener('click', () => {
+    if(pageNum !== 1) {
+        page_down();
+        submitHandle(name_input.value);
+    }
+});
 
-let success = () => {
-    if(name.value.length === 0 || name.value.length === '' ) { 
+let submitHandle = (value) => {
+    fetch_brew(value);
+    document.querySelector("#information").innerHTML = '';
 
-        // information.classList.add('hide');
-        // information.classList.remove('show');
-        // information.innerHTML = '';
-        
-       } else { 
+};
 
-        // information.classList.add('show');
-        // information.classList.remove('hide');
-        
-       }
-       
-}
+  // setup materialize components
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+    
+});
