08828b479896affa96f6b2c42bc046e2353fa075
Julian Hoyos <jhoyos135@gmail.com>
2019-02-05 13:57:22 -0500

    added google api autocomplete

--- script.js
+++ script.js
@@ -2,112 +2,179 @@
 // api used: https://www.openbrewerydb.org
 */
 
+
 window.addEventListener('DOMContentLoaded', () => {
 
-let name_input = document.querySelector('#full_name');
-let pageNum = 1;
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev');
-
-const fetch_brew = async (name) => {
-
-    let states = document.getElementById("states");
-    let state = `&by_state=${states.options[states.selectedIndex].text}`;
-
-    let url = `https://api.openbrewerydb.org/breweries?by_name=${name.toLowerCase()}${state}&page=${pageNum}&per_page=1&sort=name`;
-
-
-    // console.log(url);
+    let city_input = document.querySelector('#full_city');
+    let name_input = document.querySelector('#full_name');
+    let pageNum = 1;
+    let next = document.querySelector('.next');
+    let prev = document.querySelector('.prev');
+    let selection = document.querySelector('.selection');
     
-    let res = await fetch(url);
-    let breweries = await res.json();
+    const fetch_brew = async (full_google_name,name) => {
+    
+        // let states = document.getElementById("states");
+        // let state = `&by_state=${states.options[states.selectedIndex].text}`;
+        
+    
+        let url = `https://api.openbrewerydb.org/breweries${full_google_name}&page=${pageNum}&per_page=1&sort=name&by_name=${name}`;        
+        // console.log(url);
+        
+        let res = await fetch(url);
+        let breweries = await res.json();
+        
+        
+        if(breweries.length == 0) {
+            selection.classList.add('disabled');
+            document.querySelector("#information").textContent = 'No breweries found!'
+        } else {
+            selection.classList.remove('disabled');  
+        }
+        
+    
+    for(let i in breweries) {
+        let json = breweries[i];
+    
+            // console.log(json)
+    
+           let info =  document.querySelector("#information");
+           let id = json.id;
+           let name = json.name;
+           let brewery_type = json.brewery_type;
+           let street = json.street;
+           let city = json.city;
+           let state = json.state;
+           let postal = json.postal_code;
+           let phone = json.phone;
+           let website = json.website_url;
+           let address = `${street}, ${city}, ${state}, ${postal}`;
+           let lat = parseFloat(json.latitude);
+           let lon = parseFloat(json.longitude);
 
-for(let i in breweries) {
-    let json = breweries[i];
+           console.log(lat, lon)
+    
+           info.innerHTML += `
+        <div class="card hoverable z-depth-1">
+            <div class="card-title">
+                <h5> ${name} </h5> 
+                <p>Type: ${brewery_type} </p>
+                <p>City: ${city} </p>
+            </div>
+            <div class="card-content">
+                <div class="card-action">
+                <p>
+                  <a target="_blank" href="https://www.google.ca/maps/place/${street},+${state},+${postal}/" class="btn indigo"> Address </a>
+                </p>
+                </div>
+    
+                <div class="mt-2 card-action">
+                <p> <span><a class="btn indigo modal-trigger" href="${website}" data-target="website-${id}"> Website </a>
+                </p>
+                </div>
+    
+                <div class="text-right call">
+                <h5>
+                    <a href="tel://+1${phone}">
+                        <i class="fa fa-phone fa-2x"></i>
+                    </a>
+                </h5> 
+                </div>
+            </div>
+    
+            <!-- Modals -->
+            <div id="${id}"></div>
+           
+    </div>
+           `;
 
-        // console.log(json)
 
-       let info =  document.querySelector("#information");
-       let id = json.id;
-       let name = json.name;
-       let brewery_type = json.brewery_type;
-       let street = json.street;
-       let city = json.city;
-       let state = json.state;
-       let postal = json.postal_code;
-       let phone = json.phone;
-       let website = json.website_url;
-       let address = `${street}, ${city}, ${state}, ${postal}`;
-       let lat = parseFloat(json.latitude);
-       let lon = parseFloat(json.longitude);
-       
+        let custom_modals = document.getElementById(`${id}`);
 
-       info.innerHTML += `
-    <div class="card hoverable z-depth-1">
-        <div class="card-title">
-            <h5> ${name} </h5> 
-            <p>Type: ${brewery_type} </p>
-            <p>City: ${city} </p>
+        custom_modals.innerHTML += `
+        
+        <!-- Modal Trigger -->
+        <button class="modal-trigger indigo search" data-target="bar-${id}">
+            <i class="material-icons small indigo white-text"> gps_fixed </i>
+        </button>
+    
+            <!-- Modal Structure -->
+            <div id="bar-${id}" class="modal" style="height:90%; width: 90%; max-height: 85% !important;">
+              <div class="modal-content" style="height:90%; width: 100%; margin: 0 auto;">
+                <h3>Hotels near ${name}</h3>
+                    <hr />
+                <div id="hotel-${id}" class="hotel-flex"></div>
+        
+              </div>
+            </div>
+
+        <!-- Website modal -->
+        <div id="website-${id}" class="modal" style="height:90%; width: 90%; max-height: 85% !important;">
+          <div class="modal-content" style="height:90%; width: 100%; margin: 0 auto;">
+            <h4> ${name} - Website</h4>
+                <hr />
+            <iframe width="100%" height="100%" frameborder="0" allowtransparency="true" src="${website}"></iframe>
+    
+          </div>
         </div>
-        <div class="card-content">
-            <div class="card-action">
-            <p>
-              <a target="_blank" href="https://www.google.ca/maps/place/${street},+${state},+${postal}/" class="btn indigo"> Address </a>
-            </p>
-            </div>
+        `;
+        
 
-            <div class="mt-2 card-action">
-            <p> <span><a class="btn indigo modal-trigger" href="${website}" data-target="website-${id}"> Visit Website </a>
-            </p>
-            </div>
+    if(website === '') {
+        document.getElementById(`website-${id}`).textContent = 'No Website found!';
+    };
 
-            <div class="text-right call">
-            <h5>
-                <a href="tel://+1${phone}">
-                    <i class="fa fa-phone fa-2x"></i>
-                </a>
-            </h5> 
-            </div>
-        </div>
+    document.querySelector(`[data-target="bar-${id}"]`).addEventListener('click', () => {
+        foursquare(city,lon,lat,id);
 
-        <!-- Modals -->
-        <div id="${id}"></div>
-       
-</div>
-       `;
+    });
+
+    };
 
 
-       google(lat, lon, name, id, website);
+      // setup materialize components
+      let modals = document.querySelectorAll('.modal');
+      M.Modal.init(modals);
 
-};
-  // setup materialize components
-  let modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-
-  console.clear()
-  
-};
-
-name_input.addEventListener('keyup', () => {
+    };
     
-    pageNum = 1;
-    submitHandle(name_input.value);
+    city_input.addEventListener('keyup', () => {
+        name_input.value = '';
+        pageNum = 1;
+        if(city_input.value == '') {
+            document.querySelector("#information").innerHTML = '';
+        }
+
+    });
+    name_input.addEventListener('keyup', () => {
+
+        pageNum = 1;
+        let location_info = document.getElementById("full_city").value.split(",");
+        let cityName = location_info[0].toLowerCase().trim();
+        let stateName = location_info[1].toLowerCase().trim();
+        stateName = abbrState(stateName, 'name').toLowerCase();
+        let full_google_name = `?by_city=${cityName}&by_state=${stateName}`;
+        submitHandle(full_google_name, name_input.value);
+
+    });
     
-});
+    
+    // let states = document.getElementById("states");
+    
+    // states.onchange = function() {
+    //     let single_state = states.options[states.selectedIndex].text;
+    
+    //     pageNum = 1;
+    //     submitHandle(city_input.value, name_input.value);
+    //     fetch_weather(single_state);
+    //     selection.classList.remove('disabled');
+    //     city_input.value = '';
+
+    // };
 
 
-let states = document.getElementById("states");
+    // Pagination functionality
 
-states.onchange = function() {
-    let single_state = states.options[states.selectedIndex].text;
-
-    pageNum = 1;
-    submitHandle(name_input.value);
-    fetch_weather(single_state);
-
-};
-
-// Pagination functionality
 let page_up = () => {
     pageNum = pageNum + 1;
 };
@@ -116,27 +183,67 @@ let page_down = () => {
     pageNum = pageNum - 1;
 };
 
-next.addEventListener('click', () => {
-    page_up();
-    submitHandle(name_input.value);
 
+next.addEventListener('click', () => {
+    if(document.querySelector("#information").childElementCount === 0 ) {
+        selection.classList.add('disabled');
+    } else {
+        page_up();
+        let location_info = document.getElementById("full_city").value.split(",");
+        let cityName = location_info[0].toLowerCase().trim();
+        let stateName = location_info[1].toLowerCase().trim();
+        stateName = abbrState(stateName, 'name').toLowerCase();
+        let full_google_name = `?by_city=${cityName}&by_state=${stateName}`;
+        submitHandle(full_google_name,name_input.value);
+    }
 });
 
 prev.addEventListener('click', () => {
     if(pageNum !== 1) {
         page_down();
-        submitHandle(name_input.value);
+        let location_info = document.getElementById("full_city").value.split(",");
+        let cityName = location_info[0].toLowerCase().trim();
+        let stateName = location_info[1].toLowerCase().trim();
+        stateName = abbrState(stateName, 'name').toLowerCase();
+        let full_google_name = `?by_city=${cityName}&by_state=${stateName}`;
+        submitHandle(full_google_name,name_input.value);
+        selection.classList.remove('disabled');
     }
 });
 
-let submitHandle = (value) => {
-    fetch_brew(value);
-    document.querySelector("#information").innerHTML = '';
 
+// google autocomplete api
+var options = {
+        types: ['(cities)'],
+        componentRestrictions: { country: 'us' }
+    };
+autocomplete = new google.maps.places.Autocomplete(city_input, options);
+
+let get_map_data = () => {
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    let location_info = document.getElementById("full_city").value.split(",");
+    let cityName = location_info[0].toLowerCase().trim();
+    let stateName = location_info[1].toLowerCase().trim();
+    stateName = abbrState(stateName, 'name').toLowerCase();
+    let full_google_name = `?by_city=${cityName}&by_state=${stateName}`;
+    submitHandle(full_google_name,name_input.value);
+    // console.log(full_google_name);
+    fetch_weather(stateName);
+
+});
 };
 
-  // setup materialize components
-  let modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
+    let submitHandle = (full_google_name,name) => {
+        fetch_brew(full_google_name, name);
+        document.querySelector("#information").innerHTML = '';
     
-});
+    };
+    get_map_data();
+
+    
+      // setup materialize components
+      let modals = document.querySelectorAll('.modal');
+      M.Modal.init(modals);
+        
+    });
+    
